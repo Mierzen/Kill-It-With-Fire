7a75244753df8d81ea297068fa031ed443003cd0
TheRealMierzen <marcotaljaard@gmail.com>
2014-10-09 09:11:00 +0200

    Added some more processes

--- Kill It With Fire!/Kill It With Fire!/kill.vb
+++ Kill It With Fire!/Kill It With Fire!/kill.vb
@@ -7,7 +7,15 @@
         {"C:\Program Files\SAMSUNG\USB Drivers\25_escape\conn\ss_conn_service.exe", "SAMSUNG Mobile Connectivity Service"}, _
         {"C:\Program Files (x86)\The Panel Station\The Panel Station Application\PanelStationNotificationService.exe", "PanelManagementNotificationService"}, _
         {"C:\Program Files\NVIDIA Corporation\NvStreamSrv\nvstreamsvc.exe", "NVIDIA Streamer Service"}, _
-        {"C:\Program Files (x86)\Common Files\Microsoft Shared\Phone Tools\CoreCon\11.0\bin\IpOverUsbSvc.exe", "Windows Phone IP over USB Transport (IpOverUsbSvc)"} _
+        {"C:\Program Files (x86)\Common Files\Microsoft Shared\Phone Tools\CoreCon\11.0\bin\IpOverUsbSvc.exe", "Windows Phone IP over USB Transport (IpOverUsbSvc)"}, _
+        {"C:\Program Files (x86)\TeamViewer\Version9\TeamViewer_Service.exe", "A"}, _
+        {"C:\Program Files (x86)\Common Files\Adobe\ARM\1.0\armsvc.exe", "A"}, _
+        {"C:\Program Files\Bonjour\mDNSResponder.exe", "A"}, _
+        {"C:\Program Files (x86)\Common Files\Apple\Mobile Device Support\AppleMobileDeviceService.exe", "A"}, _
+        {"C:\Program Files (x86)\NVIDIA Corporation\3D Vision\nvSCPAPISvr.exe", "A"}, _
+        {"C:\Program Files (x86)\Google\Update\GoogleUpdate.exe", "A"}, _
+        {"C:\Program Files\iPod\bin\iPodService.exe", "A"}, _
+        {"C:\ProgramData\IePluginServices\PluginService.exe", "A"} _
         }
         For Each p In System.Diagnostics.Process.GetProcesses()
             Try
